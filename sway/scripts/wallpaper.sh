b4c5cfdb44b6139eae2b008ae7e70c2d0e068956
voidptrx <voidptrx@noreply.codeberg.org>
2026-04-04 01:11:41 +0300

    update sway config

--- sway/scripts/wallpaper.sh
+++ sway/scripts/wallpaper.sh
@@ -6,14 +6,9 @@ WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 # Get a list of all image files in the directory
 wallpapers=($WALLPAPER_DIR/*)
 
-# random_wallpaper="${wallpapers[RANDOM % ${#wallpapers[@]}]}"
-# swaymsg output "*" bg "$random_wallpaper" fill
-
-# swaymsg output "*" bg "$(find $HOME/Pictures/Wallpaper/ | shuf -n 1)" fill
-
 while true; do
   # Select a random wallpaper from the list
-  random_wallpaper="${wallpapers[RANDOM % ${#wallpapers[@]}]}"
+  random_wallpaper="${wallpapers[RANDOM%${#wallpapers[@]}]}"
 
   # Set the wallpaper using swaymsg command
   swaymsg output "*" bg "$random_wallpaper" fit
