753bf7f83d5cf30be3e16394dfc5a6ba99bdf945
voidptrx <voidptrx@noreply.codeberg.org>
2026-04-06 18:29:01 +0300

    sway: update auto-brightness script

--- sway/.config/sway/scripts/brightness.sh
+++ sway/.config/sway/scripts/brightness.sh
@@ -1,54 +1,59 @@
 #!/bin/sh
 
-BACKLIGHT="/sys/class/backlight/intel_backlight"
-STEP_DELAY=0.5
-STEP_SIZE=2
+STEP_DELAY=0.1
+STEP_SIZE=1
 
 get_profile() {
   hour=$(date +%H)
-  if [ "$hour" -ge 20 ] || [ "$hour" -lt 7 ]; then
-    echo 10
-  elif [ "$hour" -lt 9 ]; then
-    echo 35
-  elif [ "$hour" -lt 11 ]; then
-    echo 50
-  elif [ "$hour" -lt 16 ]; then
-    echo 70
-  elif [ "$hour" -lt 18 ]; then
-    echo 90
-  else
-    echo 35
-  fi
-}
-
-set_laptop() {
-  max=$(cat "$BACKLIGHT/max_brightness")
-  echo $((max * $1 / 100)) >"$BACKLIGHT/brightness"
+  case $hour in
+  20 | 21 | 22 | 23 | 00 | 01 | 02 | 03 | 04 | 05 | 06) echo 10 ;;
+  07 | 08) echo 35 ;;
+  09 | 10) echo 55 ;;
+  11 | 12 | 13 | 14 | 15) echo 85 ;;
+  16 | 17) echo 65 ;;
+  18 | 19) echo 40 ;;
+  *) echo 35 ;;
+  esac
 }
 
 get_laptop() {
-  max=$(cat "$BACKLIGHT/max_brightness")
-  cur=$(cat "$BACKLIGHT/brightness")
-  echo $((cur * 100 / max))
+  brightnessctl -m | cut -d, -f4 | tr -d '%'
 }
 
-transition() {
-  cur=$1 target=$2 setter=$3
+get_monitor() {
+  ddcutil getvcp 10 --brief 2>/dev/null | awk '{print $4}'
+}
+
+transition_laptop() {
+  local target=$1
+  cur=$(get_laptop)
   while [ "$cur" -ne "$target" ]; do
     [ "$cur" -lt "$target" ] && cur=$((cur + STEP_SIZE)) || cur=$((cur - STEP_SIZE))
-    [ "$setter" = "laptop" ] && set_laptop "$cur" || ddcutil setvcp 10 "$cur" 2>/dev/null
+    brightnessctl set "${cur}%" >/dev/null 2>&1
     sleep $STEP_DELAY
   done
+}
+
+transition_monitor() {
+  local target=$1
+  cur=$(get_monitor)
+  [ -z "$cur" ] && return
+
+  while [ $((cur - target)) -gt 2 ] || [ $((target - cur)) -gt 2 ]; do
+    [ "$cur" -lt "$target" ] && cur=$((cur + 5)) || cur=$((cur - 5))
+    ddcutil setvcp 10 "$cur" >/dev/null 2>&1
+    sleep 0.5
+  done
+  ddcutil setvcp 10 "$target" >/dev/null 2>&1
 }
 
 last=""
 while true; do
   target=$(get_profile)
   if [ "$target" != "$last" ]; then
-    transition "$(get_laptop)" "$target" "laptop" &
-    transition "$(ddcutil getvcp 10 2>/dev/null | grep -oP 'current value =\s*\K[0-9]+')" "$target" "monitor" &
-    wait
+    transition_laptop "$target" &
+    transition_monitor "$target" &
     last=$target
   fi
-  sleep 60
+  sleep 300
 done
