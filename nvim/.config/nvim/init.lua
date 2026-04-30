e1ee60554d024b939f651ecbbbd173215a17d38f
voidptrx <voidptrx@noreply.codeberg.org>
2026-04-30 11:41:27 +0300

    nvim: remove nerd font check and diagnostic module

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.g.have_nerd_font = true
 
 require("config.options")
 require("config.autocmds")
-require("config.diagnostic")
 require("config.keymaps")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
